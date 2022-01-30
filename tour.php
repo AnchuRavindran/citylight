6c7fa723604330004ba3f29c11789d0625e3219a
AnchuK <anchukariyapath@gmail.com>
2022-01-30 18:06:44 +0530

    changes

--- tour.php
+++ tour.php
@@ -122,42 +122,45 @@ include('includes/config.php');
       </div>
     </section> <!-- .section -->
 
+    <section id="footer">
     <footer class="ftco-footer ftco-bg-dark ftco-section">
       <div class="container">
         <div class="row mb-5">
           <div class="col-md">
             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">dirEngine</h2>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+				<a href="./index.php" class="logo navbar-brand"><img src="./assets/img/logo.png" alt="Logo" style="width:70%;height:100%;"></a>
+              <p>If you are looking for a reliable tour operator in Kerala “CityLight Travels” is the right choice for you.Travel to Kerala with CityLight Travels and enjoy a perfect Holiday.</p>
               <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                <li class=""><a href="#"><span class="icon-twitter"></span></a></li>
-                <li class=""><a href="#"><span class="icon-facebook"></span></a></li>
-                <li class=""><a href="#"><span class="icon-instagram"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
               </ul>
             </div>
           </div>
           <div class="col-md">
             <div class="ftco-footer-widget mb-4 ml-md-5">
-              <h2 class="ftco-heading-2">Information</h2>
+              <h2 class="ftco-heading-2">Useful Links</h2>
               <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">About</a></li>
-                <li><a href="#" class="py-2 d-block">Service</a></li>
-                <li><a href="#" class="py-2 d-block">Terms and Conditions</a></li>
-                <li><a href="#" class="py-2 d-block">Become a partner</a></li>
-                <li><a href="#" class="py-2 d-block">Best Price Guarantee</a></li>
-                <li><a href="#" class="py-2 d-block">Privacy and Policy</a></li>
+				<li><a href="./index.php" class="py-2 d-block">Home</a></li>
+                <li><a href="./about.php" class="py-2 d-block">About</a></li>
+                <li><a href="./services.php" class="py-2 d-block">Services</a></li>
+                <li><a href="./tour.php" class="py-2 d-block">Tour packages</a></li>
+                <li><a href="./gallery.php" class="py-2 d-block">Gallery</a></li>
+                <li><a href="./bus.php" class="py-2 d-block">Buses</a></li>
+                <li><a href="./contact.php" class="py-2 d-block">Contact</a></li>
               </ul>
             </div>
           </div>
           <div class="col-md">
              <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Customer Support</h2>
+              <h2 class="ftco-heading-2">Services</h2>
               <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">FAQ</a></li>
-                <li><a href="#" class="py-2 d-block">Payment Option</a></li>
-                <li><a href="#" class="py-2 d-block">Booking Tips</a></li>
-                <li><a href="#" class="py-2 d-block">How it works</a></li>
-                <li><a href="#" class="py-2 d-block">Contact Us</a></li>
+                <li><a href="./services.php" class="py-2 d-block">Holiday Package</a></li>
+                <li><a href="./services.php" class="py-2 d-block">Bus Services</a></li>
+                <li><a href="./services.php" class="py-2 d-block">Convenient Payment Method</a></li>
+                <li><a href="./services.php" class="py-2 d-block">Airline Tickets</a></li>
+                <li><a href="./services.php" class="py-2 d-block">Document Attestation</a></li>
+				<li><a href="./services.php" class="py-2 d-block">Visa Services</a></li>
               </ul>
             </div>
           </div>
@@ -165,27 +168,20 @@ include('includes/config.php');
             <div class="ftco-footer-widget mb-4">
             	<h2 class="ftco-heading-2">Have a Questions?</h2>
             	<div class="block-23 mb-3">
-	              <ul>
-	                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
-	              </ul>
+					<p>
+						Bus Stand Exit, <br>Taliparamba,Kannur<br> Kerala 670141<br><br>
+						 <a href="tel:+91 9048001100">+91 9048001100</a><br>
+						<a href="mailto:citylighttravels.kerala@gmail.com"> citylighttravels.kerala@gmail.com</a>
+				
 	            </div>
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-md-12 text-center">
-
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-          </div>
-        </div>
+       
       </div>
     </footer>
     
-  
+  </section>
 
   <!-- loader -->
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
